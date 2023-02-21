fc331a3e2216d3de39da767e07586ee4a8abcdd4
Ostmind <serz.sim@gmail.com>
2023-02-21 19:45:27 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Geometry[] geometry = {new Cylinder(2, 5,4),
-                new Cube(5,5)};
+        Geometry[] geometry = {new Cylinder(2, 5,80),
+                new Cube(5,250)};
 
         for(Geometry geom : geometry)
-            geom.getDensity();
+            System.out.println(geom.getName() + ": density = " + geom.getDensity());
     }
 }
