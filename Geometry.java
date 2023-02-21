730e6e10a20ac9a2b7ce4f9b73979d351833a4e6
Ostmind <serz.sim@gmail.com>
2023-02-21 19:45:52 +0300

    Update Geometry.java

--- Geometry.java
+++ Geometry.java
@@ -1,4 +1,13 @@
 public abstract class Geometry {
+    private double mass;
+
+    public Geometry (double mass){
+        this.mass = mass;
+    }
     public abstract String getName();
-    public abstract void getDensity();
+
+    public double getDensity(){
+        return mass / getArea();
+    };
+    public abstract double getArea ();
 }
