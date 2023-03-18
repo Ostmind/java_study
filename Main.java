35b273253d1d81dff60a66b6714b9dc6ea051eb4
Ostmind <serz.sim@gmail.com>
2023-03-18 11:21:52 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.List;
 import java.util.stream.IntStream;
 
 class Main { 
+ 
  public static void main(String[] args) {
  int[] nums = new int[]{1,2,3,4,5,6,7,8,9,10};
  int sum = IntStream.range(0, nums.length).filter(index -> index % 2 == 0).map(index -> nums[index]).sum();
