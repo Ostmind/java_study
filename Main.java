af0a0f8e729cf91adfb4733e667a93dc41510d41
Ostmind <serz.sim@gmail.com>
2023-03-20 23:46:01 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,73 +1,71 @@
 class CounterObject
 {
-    int counter = 0;
     
-    public void increment() {
-     counter++;
+    private static volatile Integer counter = 0;
+    
+    private CounterObject(){}
+    
+    public static Integer increment() {    
+     synchronized(CounterObject.class){
+     counter+=1;
+     return counter;
+     }
     }
     
-    public void decrement() {
-     counter--;
+    public static Integer getCounter() {
+     return counter;
+    }
+    
+    public static void decrement() {
+     counter -=1;
     }
 }
 
 class ProcessingThread implements Runnable {
 
-    private CounterObject res;
-    ProcessingThread(CounterObject res)
-    {
-        this.res = res;
-    }    
+    
+    private static final Object lock = new Object();
     
     @Override
     public void run() {
-        while (res.counter <= 100) {
-             synchronized(res) {
-              res.increment();
-             }
-             System.out.println(Thread.currentThread().getName() + " " + res.counter);
-             synchronized(res) {
-             if (res.counter == 100)
+        while (CounterObject.getCounter() <= 100) {
+             
+             Integer increment = CounterObject.increment();
+             synchronized(lock) {
+             if (increment == 100)
              {
               
               try {
-               wait();
+               lock.wait();
               }
               catch (InterruptedException e) {}
               System.out.println("I'm winner " + Thread.currentThread().getName());
-              //break;
+              
              }
-             if (res.counter > 100)
+             if (increment > 100)
              {
               System.out.println("I'm late.... " + Thread.currentThread().getName());
-              notify();
-              // res.decrement();
-              //break;
+              lock.notify();
+              
              }
           
             }
         }
     }
-   
-
-    public int getCount() {
-        return this.res.counter;
-    } 
 }
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        CounterObject counterObject = new CounterObject();
-        ProcessingThread pt = new ProcessingThread(counterObject);
+        
+        ProcessingThread pt = new ProcessingThread();
         Thread t1 = new Thread(pt, "t1");
         t1.start();
         Thread t2 = new Thread(pt, "t2");
         t2.start();
         t1.join();
         t2.join();
-        //System.out.println("I'm late " + pt.getLooser());
-        //System.out.println("I'm winner " + pt.getWinner());
-        System.out.println("Processing count=" + pt.getCount());
+        CounterObject.decrement();
+        System.out.println("Processing count=" + CounterObject.getCounter());
     }
 }
